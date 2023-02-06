627ed7214c17f3aefd350e8457ac365d1876bdf6
kauamath <medeiroskaua372@gmail.com>
2023-02-06 12:50:14 -0400

    add current weather data API

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ let key = "5414542d9e0a55b4a6e025d9e5c36fb6";
 
 function putOnScreen(data){
 
-    console.log(data)
-
     document.querySelector(".city").innerHTML = "Tempo em " + data.name
     document.querySelector(".temp").innerHTML = Math.floor(data.main.temp)  + "°C"
     document.querySelector(".description").innerHTML = data.weather[0].description
