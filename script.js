4af1aba86110340a4c02f35213cc925a38ce659f
kauamath <medeiroskaua372@gmail.com>
2023-02-09 12:16:58 -0400

    added: sensation, max and min time

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let key = "5414542d9e0a55b4a6e025d9e5c36fb6";
 
-function putOnScreen(data){
-
-    document.querySelector(".city").innerHTML = "Tempo em " + data.name
-    document.querySelector(".temp").innerHTML = Math.floor(data.main.temp)  + "°C"
-    document.querySelector(".description").innerHTML = data.weather[0].description
-    document.querySelector(".umidity").innerHTML = "Umidade: " + data.main.humidity + "%"
-    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png"
-    
+function putOnScreen(data) {
+  console.log(data)
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.floor(data.main.temp) + "°C";
+  document.querySelector(".description").innerHTML =data.weather[0].description;
+  document.querySelector(".icon").src ="https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+  document.querySelector(".temp-max").innerHTML ="Temp-max: " + Math.floor (data.main.temp_max) + "°C";
+  document.querySelector(".sensation").innerHTML ="Sensação: " + Math.floor (data.main.feels_like) + "°C";
+  document.querySelector(".umidity").innerHTML ="Umidade: " + data.main.humidity + "%";
+  document.querySelector(".temp-min").innerHTML ="Temp-min: " + Math.floor (data.main.temp_min) + "°C";
 }
 
 async function seekCity(city) {
@@ -20,7 +22,7 @@ async function seekCity(city) {
       "&units=metric"
   ).then((response) => response.json());
 
-  putOnScreen(data)
+  putOnScreen(data);
 }
 
 function clickButton() {
